c5d570759cde81a82d2ead0183970aa50ee27e5d
Will Vanderhoef <william.vanderhoef@gmail.com>
2011-09-01 01:47:18 -0400

    Updated AllTests to include lists.

--- src/test/com/williamvanderhoef/foursquare/AllTests.java
+++ src/test/com/williamvanderhoef/foursquare/AllTests.java
@@ -5,10 +5,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.williamvanderhoef.foursquare.test.CheckinsTest;
+import com.williamvanderhoef.foursquare.test.ListsTest;
 import com.williamvanderhoef.foursquare.test.VenueHistoryTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({CheckinsTest.class, VenueHistoryTest.class})
+@SuiteClasses({CheckinsTest.class, VenueHistoryTest.class, ListsTest.class})
 public class AllTests {
 
 }
