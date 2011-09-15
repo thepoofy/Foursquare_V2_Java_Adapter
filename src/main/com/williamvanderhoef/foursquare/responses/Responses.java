2f2c12de35cb1280159b81a6753beff30b1de371
Will Vanderhoef <william.vanderhoef@gmail.com>
2011-09-15 02:04:57 -0400

    Simplifying the responses object.

--- src/main/com/williamvanderhoef/foursquare/responses/Responses.java
+++ src/main/com/williamvanderhoef/foursquare/responses/Responses.java
@@ -1,38 +1,17 @@
 package com.williamvanderhoef.foursquare.responses;
 
-import java.lang.reflect.Type;
+import java.util.List;
 
-import com.williamvanderhoef.foursquare.adapters.DefinedType;
-import com.williamvanderhoef.foursquare.model.subtypes.Results;
 
-public class Responses<
-		T1 extends Results<?>,
-		T2 extends Results<?>
-	> implements GetResponseBase, DefinedType{
-
-	private T1 result1;
-	private T2 result2;
-
+public class Responses
+{
+	private List<String> responses;
 	
-
-	@Override
-	public Type defineType() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> getResponses() {
+		return responses;
 	}
 
-
-
-	public T1 getResult1() {
-		return result1;
-	}
-	public void setResult1(T1 result1) {
-		this.result1 = result1;
-	}
-	public T2 getResult2() {
-		return result2;
-	}
-	public void setResult2(T2 result2) {
-		this.result2 = result2;
+	public void setResponses(List<String> responses) {
+		this.responses = responses;
 	}
 }
