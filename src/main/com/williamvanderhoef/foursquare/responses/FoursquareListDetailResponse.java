154eb2b5080a95f1aece803844d00cadca47d025
william.vanderhoef@gmail.com <william.vanderhoef@gmail.com>
2011-09-17 19:19:23 -0400

    Added getters.

--- src/main/com/williamvanderhoef/foursquare/responses/FoursquareListDetailResponse.java
+++ src/main/com/williamvanderhoef/foursquare/responses/FoursquareListDetailResponse.java
@@ -5,4 +5,22 @@ import com.williamvanderhoef.foursquare.model.FoursquareList;
 public class FoursquareListDetailResponse
 {
 	private FoursquareList list;
+
+	/**
+	 * @return the list
+	 */
+	public FoursquareList getList()
+	{
+		return list;
+	}
+
+	/**
+	 * @param list the list to set
+	 */
+	public void setList(FoursquareList list)
+	{
+		this.list = list;
+	}
+	
+	
 }
